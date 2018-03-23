2ebbac3cec8b4491d9943544faa59afd88e3e98f
Ryan Hu <ryanhuuu@gmail.com>
2018-03-23 22:07:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,17 @@
 
 def key_for_min_value(name_hash)
   min = 0
-  name_hash.each do
+  name_hash.each do |key, val|
+    if min == 0
+      min = val
+    end
+    
+    if val < min
+      min = fal
+    end
+    
+    
+  end
+  
+  return min
 end
